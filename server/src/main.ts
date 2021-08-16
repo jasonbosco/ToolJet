c9a74d3bdbe9366979ed3d899e88f2888e8af684
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2021-08-16 11:49:01 +0530

    setup swagger module

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 const fs = require('fs');
 
 globalThis.TOOLJET_VERSION = fs.readFileSync('./.version', 'utf8');
@@ -10,6 +11,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn'],
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('ToolJet')
+    .setDescription('The tooljet API endpoints')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.setGlobalPrefix('api');
   await app.enableCors();
